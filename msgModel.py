21ed9ab8e0cbaa0cc5e46b41a9aef2ffa5792ff2
LAPTOP-MOONIGHT\user <s108321027@mail1.ncnu.edu.tw>
2022-11-21 16:08:00 +0800

    model update subscriptHistory

--- msgModel.py
+++ msgModel.py
@@ -1,37 +1,54 @@
 #!C:\Users\HappyUser\AppData\Local\Programs\Python\Python38\python.exe
-#-*- coding: utf-8 -*-
-#連線DB
+# -*- coding: utf-8 -*-
+# 連線DB
 from dbConfig import conn, cur
 
-def getList(): # 取得商品屬性
-	#查詢
-	sql="select id, name, firstPrice, deadline, nowPrice from 上架 order by id;"
-	cur.execute(sql)
-	
-	records = cur.fetchall()
-	#return records
-	ret=[]
-	
-	for (id, name, firstPrice, deadline, nowPrice) in records:
-		temp={
-			'id': id,
-			'name': name,
-			'firstPrice': firstPrice,
-			'deadline': deadline,
-			'nowPrice': nowPrice
-		}
-		#print(temp)
-		ret.append(temp)
-		
-	return ret
-	
-def subscript(uid, product_id, price): #下標
-	sql = "insert into 下標 (UID, pruduct_id, price) values (%s, %s, %s)"
-	cur.execute(sql, (uid, product_id, price))
-	conn.commit()
-	return True
+
+def getList():  # 取得商品屬性
+    # 查詢
+    sql = "select id, name, firstPrice, deadline, nowPrice from 上架 order by id;"
+    cur.execute(sql)
+
+    records = cur.fetchall()
+    # return records
+    ret = []
+
+    for (id, name, firstPrice, deadline, nowPrice) in records:
+        temp = {
+            'id': id,
+            'name': name,
+            'firstPrice': firstPrice,
+            'deadline': deadline,
+            'nowPrice': nowPrice
+        }
+        # print(temp)
+        ret.append(temp)
+
+    return ret
+
+
+def subscript(uid, product_id, price):  # 下標
+    sql = "insert into 下標 (UID, product_id, price) values (%s, %s, %s)"
+    cur.execute(sql, (uid, product_id, price))
+    conn.commit()
+    return True
+
 
 def subscriptHistory(uid):
-	sql = "select id, product_id, price, time, 成功 from 下標 where UID = %s"
-	cur.execute(sql, ([uid]))
-	records = cur.fetchall()
+    sql = "select id, product_id, price, time, 成功 from 下標 where UID = %s"
+    cur.execute(sql, ([uid]))
+    records = cur.fetchall()
+
+    ret = []
+
+    for (id, product_id, price, time, 成功) in records:
+        temp = {
+            'id': id,
+            'product_id': product_id,
+			'price': price,
+			'time': time,
+			'success': 成功
+        }
+        ret.append(temp)
+
+    return ret

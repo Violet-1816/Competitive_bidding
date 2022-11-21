34a842b58fba7b859978279b89196ca4ba21532a
HuangEgg <92704517+HuangEgg@users.noreply.github.com>
2022-11-21 15:21:03 +0800

    d
    d

--- competitive_bidding.sql
+++ competitive_bidding.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- 主機： 127.0.0.1
--- 產生時間： 2022-11-21 08:09:07
--- 伺服器版本： 10.4.22-MariaDB
--- PHP 版本： 8.1.2
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2022 at 08:17 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- 資料庫: `competitive_bidding`
+-- Database: `1121`
 --
 
 -- --------------------------------------------------------
 
 --
--- 資料表結構 `上架`
+-- Table structure for table `上架`
 --
 
 CREATE TABLE `上架` (
@@ -35,19 +35,25 @@ CREATE TABLE `上架` (
   `nowPrice` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
--- --------------------------------------------------------
-
 --
--- 資料表結構 `下標`
+-- Indexes for dumped tables
 --
 
-CREATE TABLE `下標` (
-  `id` int(11) NOT NULL,
-  `UID` int(11) NOT NULL,
-  `price` int(11) NOT NULL,
-  `time` time NOT NULL,
-  `成功` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+--
+-- Indexes for table `上架`
+--
+ALTER TABLE `上架`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `上架`
+--
+ALTER TABLE `上架`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

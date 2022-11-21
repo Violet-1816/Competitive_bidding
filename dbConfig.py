11b456f75b751f36f79d88a9e697fc7e89a98333
HuangEgg <92704517+HuangEgg@users.noreply.github.com>
2022-11-21 15:28:35 +0800

    e
    e

---
+++ dbConfig.py
@@ -0,0 +1,21 @@
+#!C:\Users\HappyUser\AppData\Local\Programs\Python\Python38\python.exe
+#-*- coding: utf-8 -*-
+
+#import mariadb
+import mysql.connector 
+
+try:
+ 	#conn = mariadb.connect(
+	conn = mysql.connector.connect(
+		user="root",
+		password="",
+		host="127.0.0.1",
+		port=3306,
+		database="mydb_0919"
+	)
+except: #mariadb.Error as e:
+	print("Error connecting to DB")
+	exit(1)
+
+cur=conn.cursor() #use array to output query results
+#cur = conn.cursor(dictionary=True) #use dict to output query results

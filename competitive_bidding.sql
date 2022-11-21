565b7e4944ab7eaa32452ec0de40a9f34ab2fca3
HuangEgg <92704517+HuangEgg@users.noreply.github.com>
2022-11-21 15:23:26 +0800

    f
    f

--- competitive_bidding.sql
+++ competitive_bidding.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 21, 2022 at 08:17 AM
+-- Generation Time: Nov 21, 2022 at 08:23 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -35,6 +35,21 @@ CREATE TABLE `上架` (
   `nowPrice` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `下標`
+--
+
+CREATE TABLE `下標` (
+  `id` int(11) NOT NULL,
+  `UID` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `time` time NOT NULL,
+  `成功` tinyint(1) NOT NULL,
+  `product_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 --
 -- Indexes for dumped tables
 --
@@ -46,6 +61,12 @@ ALTER TABLE `上架`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `下標`
+--
+ALTER TABLE `下標`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -53,6 +74,12 @@ ALTER TABLE `上架`
 -- AUTO_INCREMENT for table `上架`
 --
 ALTER TABLE `上架`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `下標`
+--
+ALTER TABLE `下標`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
